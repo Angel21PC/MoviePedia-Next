3c6aeefea3147cd4a8b5a799f714bc568fe25471
Angel21PC <angelutrillas21@gmail.com>
2021-04-10 12:06:44 +0200

    antes de la api

---
+++ components/Banner/index.tsx
@@ -0,0 +1,19 @@
+export interface BannerProps {
+    
+}
+ 
+const Banner: React.SFC<BannerProps> = () => {
+    
+    const base_Url: string = 'https://image.tmdb.org/t/p/original/';
+
+    return ( 
+        <>
+
+
+
+
+        </>
+     );
+}
+ 
+export default Banner;
