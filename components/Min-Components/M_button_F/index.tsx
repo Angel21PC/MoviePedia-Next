715fe3ced76fb71cc3a5462e3cf460470fabc89a
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-12 14:04:12 +0200

    hora

--- components/Min-Components/M_button_F/index.tsx
+++ components/Min-Components/M_button_F/index.tsx
@@ -13,28 +13,41 @@ const M_B_F: React.SFC<M_B_FProps> = ({movie}) => {
 
     const [h, setH] = useState('heart');
     const [b, setB] = useState('bookmark');
+    const [e, setE] = useState('eye');
 
     async function handleSelect(e){
-        // switch (e) {
-        //     case 'heart':
-        //         await a_heart();
-        //         break;
-        //     case 'bookmark':
-        //         await a_bookmark();
-        //         break;
-        //     case 'eye':
-        //         console.log(e);
-        //         break;
-        //     default:
-        //         break;
-        // }
+        switch (e) {
+            case 'heart':
+                await a_heart();
+                break;
+            case 'bookmark':
+                await a_bookmark();
+                break;
+            case 'eye':
+                await a_eye();
+                break;
+            default:
+                break;
+        }
     }
+
+    async function a_heart(){
+        //funcion de firebase 
+    }
+    async function a_bookmark(){
+        
+    }
+    async function a_eye(){
+        
+    } 
+
+
     return ( 
         <>
             <h3>{movie.title}</h3>
             <FontAwesomeIcon className="icon fa-2x" id={h} name="heart" icon={faHeart} onClick={()=>handleSelect('heart')}/>
             <FontAwesomeIcon className="icon fa-2x" id={b} name="bookmark" icon={faBookmark} onClick={()=>handleSelect('bookmark')}/>
-            <FontAwesomeIcon className="icon fa-2x" id="eye" name="eye" icon={faEye} onClick={()=>handleSelect('eye')}/>
+            <FontAwesomeIcon className="icon fa-2x" id={e} name="eye" icon={faEye} onClick={()=>handleSelect('eye')}/>
         </>
      );
 }
