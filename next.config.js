eecc6c1be10441b7ffc2d34dd191e8d9ef7fe66c
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-22 12:32:34 +0200

    CORS te mato

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "https://movie-pedia-next-e0dkngyfb-angel21pc.vercel.app/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
