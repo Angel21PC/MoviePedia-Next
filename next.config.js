1d66b1eac40d880c1f527641b005efd68e304f11
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-22 12:30:15 +0200

    CORS te voy a..

--- next.config.js
+++ next.config.js
@@ -4,14 +4,19 @@ module.exports = withImages({
     return config
   }
 })
-
 module.exports = {
-  async rewrites() {
-      return [
-        {
-          source: 'http://localhost:3000',
-          destination: 'https://movie-pedia-next-hkzl3fqlc-angel21pc.vercel.app',
-        },
-      ]
-    },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
