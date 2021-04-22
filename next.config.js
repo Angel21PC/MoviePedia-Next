d1cd7b0df8341e10a05f7dc6ebac13cd2fb93114
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-22 12:46:27 +0200

    CORS te odio

--- next.config.js
+++ next.config.js
@@ -4,19 +4,3 @@ module.exports = withImages({
     return config
   }
 })
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "https://movie-pedia-next-e0dkngyfb-angel21pc.vercel.app/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
