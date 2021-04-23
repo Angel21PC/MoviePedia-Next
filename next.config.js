55bb6170f97257c919e27e47716b8b1fd527b5d3
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-23 12:19:54 +0200

    FF

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: 'https://movie/api/:path*',
-        destination: 'https://movie-pedia-next-e0dkngyfb-angel21pc.vercel.app/api/:path*' // Proxy to Backend
-      }
-    ]
-  }
-}
-
-
-
 const withImages = require('next-images')
 module.exports = withImages({
   webpack(config, options) {
