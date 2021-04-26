7171d79458329e99c997ca42f2dc247335336520
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-26 11:16:56 +0200

    cors i hate you .

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ module.exports = withImages({
     return config
   }
 })
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(proxy('/api', {
+        target: 'https://movie-pedia-next-e0dkngyfb-angel21pc.vercel.app',
+        logLevel: 'debug',
+        changeOrigin: true
+    }));
+};
