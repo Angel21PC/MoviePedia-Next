65146aeb896db57e42fd68822febe1e72c2305ac
Ángel Utrillas Marín <autrillas@centauro.net>
2021-04-26 10:35:42 +0200

    cors i hate you

--- pages/api/movie/PopularM.ts
+++ pages/api/movie/PopularM.ts
@@ -8,6 +8,10 @@ export default async (
   
   const page:number = req.query.p
   // console.log(page)
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   try {
     const request = await axios.get(r.fetchPopular_Movie+`&page=${page}`);
     res.status(200).json({data: request.data})
