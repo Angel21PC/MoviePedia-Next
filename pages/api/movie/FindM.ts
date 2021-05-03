56335616024a1d7e99833388ed3d37e932707026
Ángel Utrillas Marín <autrillas@centauro.net>
2021-05-03 12:52:26 +0200

    vercel tu puedes

--- pages/api/movie/FindM.ts
+++ pages/api/movie/FindM.ts
@@ -8,7 +8,13 @@ export default async (
   const text:string = req.query.text
 
   try {
-    const request = await axios.get(r.fetchFindMovie.concat(`&query=`+text));
+    const request = await axios.get(r.fetchFindMovie.concat(`&query=`+text),{
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+      },
+    });
     res.status(200).json({data: request.data})
   } catch (error) {
     res.status(200).json(error)
