c0b04cb2689e8885c2e468199f1d512784014d26
Ángel Utrillas Marín <autrillas@centauro.net>
2021-05-04 08:21:19 +0200

    vamo a ver

--- pages/all_pages/Movies/index.tsx
+++ pages/all_pages/Movies/index.tsx
@@ -1,4 +1,7 @@
 import { NextPage } from "next";
+import { GetStaticProps } from "next";
+import api from "../../api/index";
+import DataCache from "../../../util/DataCache";
 
 //initialprops
 import { URL, api_rutesM } from "../../../firebase/config/rute_api";
@@ -7,6 +10,32 @@ import { URL, api_rutesM } from "../../../firebase/config/rute_api";
 import NavBar from "../../../components/Top-Components/NavBar/index";
 import Banner from "../../../components/Top-Components/Banner/index";
 import Explorer from "../../../components/Top-Components/Explorer/index";
+
+const genresCache = new DataCache(api.genres, false, 60 * 24);
+const popularMoviesCache = new DataCache(api.popularMovies, false, 10);
+const nowPlayingMoviesCache = new DataCache(api.nowPlayingMovies, false, 10);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const [
+    genresResponse,
+    popularMoviesResponse,
+    nowPlayingMoviesResponse,
+  ] = await Promise.all([
+    genresCache.getData(),
+    popularMoviesCache.getData(),
+    nowPlayingMoviesCache.getData(),
+  ]);
+  const [movieGenres, tvGenres] = genresResponse;
+  const genres = [...movieGenres.genres, ...tvGenres.genres];
+
+  const popularMovies = popularMoviesResponse.results.map(
+    transformMovie(genres)
+  );
+  const nowPlayingMovies = nowPlayingMoviesResponse.results.map(
+    transformMovie(genres)
+  );
+  return { props: { popularMovies, nowPlayingMovies }, revalidate: 3600 };
+};
 
 export interface MovieProps {
   data: any;
