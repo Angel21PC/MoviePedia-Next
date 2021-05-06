fb7d732286759c1c0a6848b5e4a2b9a0337b1520
Ángel Utrillas Marín <autrillas@centauro.net>
2021-05-06 09:16:49 +0200

    si ?

--- pages/all_pages/TvShows/index.tsx
+++ pages/all_pages/TvShows/index.tsx
@@ -2,24 +2,37 @@ import { NextPage } from "next";
 
 //initialprops
 import { URL, api_rutesTv } from "../../../firebase/config/rute_api";
+import { GetStaticProps } from "next";
 
 //components
 import NavBar from "../../../components/Top-Components/NavBar/index";
 import Banner from "../../../components/Top-Components/Banner/index";
 import Explorer from "../../../components/Top-Components/Explorer/index";
 
+export const getStaticProps: GetStaticProps = async () => {
+  const popularMovies = await fetch(URL + api_rutesTv.Popular)
+    .then((res) => res.json())
+    .then((response) => {
+      return response;
+    });
+  return {
+    props: { popularMovies },
+    revalidate: 3600,
+  };
+};
+
 export interface TVProps {
-  data: any;
+  popularMovies: any;
 }
 
-const TV: NextPage<TVProps> = ({ data }) => {
-  console.log(data);
+const TV: NextPage<TVProps> = ({ popularMovies }) => {
+  console.log(popularMovies);
   return (
     <>
       <NavBar />
-      <Banner data={data} />
+      <Banner data={popularMovies.data} />
       <Explorer
-        URL={URL}
+        initialData={popularMovies.data}
         api_rutes={api_rutesTv}
         m_s={"/all_pages/Show_select"}
       />
@@ -35,12 +48,12 @@ const TV: NextPage<TVProps> = ({ data }) => {
 //     });
 // };
 
-async function getStaticProps() {
-  return await fetch(URL + api_rutesTv.Popular)
-    .then((res) => res.json())
-    .then((response) => {
-      return response;
-    });
-}
+// async function getStaticProps() {
+//   return await fetch(URL + api_rutesTv.Popular)
+//     .then((res) => res.json())
+//     .then((response) => {
+//       return response;
+//     });
+// }
 
 export default TV;
