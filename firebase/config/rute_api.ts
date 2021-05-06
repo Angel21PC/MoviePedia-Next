49990c5d44ceb51b9a7f79c5e64260ed61ea44e9
Ángel Utrillas Marín <autrillas@centauro.net>
2021-05-06 09:56:25 +0200

    teemo

--- firebase/config/rute_api.ts
+++ firebase/config/rute_api.ts
@@ -1,28 +1,28 @@
 import { IApi_rutesM, IApi_rutesTV } from "../../types";
 
-export const URL: string = "http://localhost:3000";
+export const URL:string = 'https://movie-pedia-next-e0dkngyfb-angel21pc.vercel.app';
 
 export const api_rutesM: IApi_rutesM = {
-  Popular: "/api/movie/PopularM",
-  Top: "/api/movie/TopM",
-  Upcoming: "/api/movie/UpcomingM",
-  Discover: "/api/movie/DiscoverM",
-  Genre: "/api/movie/GenreM",
-  Find: "/api/movie/FindM",
+    Popular: '/api/movie/PopularM',
+    Top:'/api/movie/TopM',
+    Upcoming: '/api/movie/UpcomingM',
+    Discover: '/api/movie/DiscoverM',
+    Genre: '/api/movie/GenreM',
+    Find: '/api/movie/FindM',
 
-  OneMovie: "/api/movie/GetMovie",
-  Cast: "/api/movie/GetCast",
-  Provider: "/api/movie/GetProvider",
-  Video: "/api/movie/GetVideo",
-  Similar: "/api/movie/SimilarM",
-};
+    OneMovie: '/api/movie/GetMovie',
+    Cast: '/api/movie/GetCast',
+    Provider: '/api/movie/GetProvider',
+    Video: '/api/movie/GetVideo',
+    Similar: '/api/movie/SimilarM',
+}
 
 export const api_rutesTv: IApi_rutesTV = {
-  Popular: "/api/tv/PopularTV",
-  Top: "/api/tv/TopTV",
-  Upcoming: "/api/tv/UpcomingTV",
-  Discover: "/api/tv/DiscoverTV",
-  Genre: "/api/tv/GenreTV",
+    Popular: '/api/tv/PopularTV',
+    Top:'/api/tv/TopTV',
+    Upcoming: '/api/tv/UpcomingTV',
+    Discover: '/api/tv/DiscoverTV',
+    Genre: '/api/tv/GenreTV',
 
-  OneMovie: "/api/tv/GetMovie",
-};
+    OneMovie: '/api/tv/GetMovie',
+}
