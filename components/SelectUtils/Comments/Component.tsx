ee2bc90eee1179189f83cb430290a5229c7bb02a
Ángel Utrillas Marín <autrillas@centauro.net>
2021-05-18 08:33:38 +0200

    comentarios Vercel deploy

--- components/SelectUtils/Comments/Component.tsx
+++ components/SelectUtils/Comments/Component.tsx
@@ -2,14 +2,11 @@ import React, { FC } from "react";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faBookmark, faEye } from "@fortawesome/free-solid-svg-icons";
-import {
-  Toast,
-  ToastBody,
-  ToastHeader,
-} from "react-bootstrap";
+import { Toast, ToastBody, ToastHeader } from "react-bootstrap";
 import style from "./Comments.module.scss";
 import { useAuth } from "../../../firebase/AuthContext";
-
+//Notification
+import { store } from "react-notifications-component";
 export interface CommentItemProps {
   com: any;
   id_film: string;
@@ -24,11 +21,13 @@ const CommentItem: FC<CommentItemProps> = ({ id_film, com }) => {
   let bool = false;
   useEffect(() => {
     const c = () => {
-      com.newComent.userLikes?.map((user) =>
-        user === currentUser.currentUser.email ? (bool = true) : bool
-      );
-      if (bool === true) {
-        setH("heartcheck");
+      if (currentUser.currentUser !== null) {
+        com.newComent.userLikes?.map((user) =>
+          user === currentUser.currentUser.email ? (bool = true) : bool
+        );
+        if (bool === true) {
+          setH("heartcheck");
+        }
       }
     };
     c();
@@ -52,6 +51,20 @@ const CommentItem: FC<CommentItemProps> = ({ id_film, com }) => {
       }
       console.log("mira aqui");
       console.log(response);
+    } else {
+      store.addNotification({
+        title: "Sorry",
+        message: "You need to be login",
+        type: "info",
+        insert: "top",
+        container: "top-center",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOutUp"],
+        dismiss: {
+          duration: 2000,
+          touch: true,
+        },
+      });
     }
   };
 
@@ -60,13 +73,16 @@ const CommentItem: FC<CommentItemProps> = ({ id_film, com }) => {
       <ToastHeader closeButton={false}> {com.newComent.data.user}</ToastHeader>
       <div className="d-flex w-100">
         <ToastBody className="w-70">{com.newComent.data.text}</ToastBody>
-        <FontAwesomeIcon
-          id={h}
-          className="icon fa-2x w-100"
-          name="heart"
-          icon={faHeart}
-          onClick={() => like()}
-        />
+        <div>
+          {com.newComent?.userLikes.length}
+          <FontAwesomeIcon
+            id={h}
+            className="icon fa-2x w-100"
+            name="heart"
+            icon={faHeart}
+            onClick={() => like()}
+          />
+        </div>
       </div>
     </Toast>
   );
