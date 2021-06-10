02866fc1abce4fc2d7f9b099517981e731896dce
Ángel Utrillas Marín <autrillas@centauro.net>
2021-06-10 16:48:40 +0200

    saeason  mas mejor

--- components/TvShowComponents/Season/index.tsx
+++ components/TvShowComponents/Season/index.tsx
@@ -7,7 +7,7 @@ import { URL, api_rutesTv } from "../../../config/rute_api";
 import ItemSeason from "./item";
 export interface SeasonProps {
   id: string;
-  season: [];
+  seasons: [];
 }
 
 const Season: FC<SeasonProps> = (props) => {
@@ -15,11 +15,12 @@ const Season: FC<SeasonProps> = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      {props.season?.map((s) => (
+    <div>
+      <h2 className="mb-5">Season</h2>
+      {props.seasons?.map((s) => (
         <ItemSeason {...s} />
       ))}
-    </>
+    </div>
   );
 };
 
--- components/TvShowComponents/Season/item.tsx
+++ components/TvShowComponents/Season/item.tsx
@@ -1,17 +1,27 @@
 import React, { FC, useEffect, useState } from "react";
-
+import { Collapse } from "react-bootstrap";
 export interface ItemSeasonProps {
-  title: any;
+  name: any;
   overview: any;
   air_date: any;
   episode_count: any;
 }
 
 const ItemSeason: FC<ItemSeasonProps> = (props) => {
-  const { title, overview, air_date, episode_count } = props;
+  const { name, overview, air_date, episode_count } = props;
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      <div></div>
+    <div onClick={() => setOpen(!open)} className="border border-dark rounded">
+      <div className="d-flex mt-1">
+        <h4 className="mr-3 pr-3">{name}</h4>
+        <h3 className="ml-3 pl-3">Episode Count:{episode_count}</h3>
+      </div>
+      <Collapse in={open} className="border border-dark border-bottom-0">
+        <div>
+          {overview == "" ? "Sorry, the overview is not defined" : overview}
+        </div>
+      </Collapse>
     </div>
   );
 };
