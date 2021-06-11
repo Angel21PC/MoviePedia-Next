ed71dc9d2e301420b1ec9be51b6391c4f56588d1
Ángel Utrillas Marín <autrillas@centauro.net>
2021-06-11 12:44:25 +0200

    no me renta

--- components/SelectUtils/Poster/index.tsx
+++ components/SelectUtils/Poster/index.tsx
@@ -1,9 +1,7 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 //components
-// import Image from "react-bootstrap/Image";
-import Image from "next/image";
+import Image from "react-bootstrap/Image";
 
-import styles from "./Poster.module.scss";
 export interface PosterProps {
   c: string;
   movie: any;
@@ -14,29 +12,21 @@ const Poster: FC<PosterProps> = ({ movie, c }) => {
 
   if (movie?.poster_path != null) {
     return (
-      <div className={c}>
-        <Image
-          width={200}
-          height={300}
-          className={styles.border}
-          src={`${base_Url}${movie?.poster_path}`}
-          alt={movie?.name}
-          key={movie?.name}
-        />
-      </div>
+      <Image
+        className={c}
+        src={`${base_Url}${movie?.poster_path}`}
+        alt={movie?.name}
+        key={movie?.name}
+      />
     );
   } else {
     return (
-      <div className={c}>
-        <Image
-          width={200}
-          height={300}
-          className={c}
-          src="/file-not-found.png"
-          alt={movie?.name}
-          key={movie?.name}
-        />
-      </div>
+      <Image
+        className={c}
+        src="/file-not-found.png"
+        alt={movie?.name}
+        key={movie?.name}
+      />
     );
   }
 };
